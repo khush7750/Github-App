8da58b905c5f2ec13167b55cfd1bf0d587c007a3
khush7750 <meherkhushbant@gmail.com>
2020-11-09 00:07:02 +0530

    Updates

---
+++ precache-manifest.5584197231fe2f24094fc5cc15dd0824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acee0a4cc124109867390cb4ad706c4",
+    "url": "/Github-App/index.html"
+  },
+  {
+    "revision": "9f6fc2c19cf972781a6f",
+    "url": "/Github-App/static/css/2.42f58b89.chunk.css"
+  },
+  {
+    "revision": "48bda4eecd7e6ca2693b",
+    "url": "/Github-App/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9f6fc2c19cf972781a6f",
+    "url": "/Github-App/static/js/2.d3dbd877.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/Github-App/static/js/2.d3dbd877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bda4eecd7e6ca2693b",
+    "url": "/Github-App/static/js/main.6a7046a4.chunk.js"
+  },
+  {
+    "revision": "b40626742422e4e5d173",
+    "url": "/Github-App/static/js/runtime-main.e19d0119.js"
+  }
+]);
